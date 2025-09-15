08580e340abf56b29f0a97c6c8a3e67e62270f03
reactmore <reactmorecom@gmail.com>
2025-09-15 18:22:51 +0700

    feat(solana): enhance transfer function with improved transaction handling and amount parsing

--- src/common/helpers/solanaHelper.ts
+++ src/common/helpers/solanaHelper.ts
@@ -4,6 +4,7 @@ import {
   getOrCreateAssociatedTokenAccount,
   transfer as transferToken,
   getMint,
+  createTransferInstruction
 } from '@solana/spl-token';
 import {
   BalancePayload,
@@ -18,7 +19,7 @@ import {
   TransferPayload,
 } from '../utils/types';
 import * as bs58 from 'bs58';
-import { successResponse } from '../utils';
+import { successResponse, parseAmount, formatAmount } from '../utils';
 import * as bip39 from 'bip39';
 import { derivePath } from 'ed25519-hd-key';
 // @ts-ignore
@@ -39,7 +40,6 @@ export const chainId = {
 
 const getConnection = (rpcUrl?: string) => {
   const connection = provider(rpcUrl);
-
   return connection;
 };
 
@@ -134,28 +134,6 @@ const getBalance = async (args: BalancePayload): Promise<IResponse> => {
   }
 };
 
-const waitForTransaction = async (
-  connection: solanaWeb3.Connection,
-  signature: string,
-  retries = 15,
-  delay = 2000
-) => {
-  for (let i = 0; i < retries; i++) {
-    const resp = await connection.getSignatureStatuses([signature]);
-    const status = resp.value[0];
-
-    if (status?.confirmationStatus === "finalized") {
-      return await connection.getTransaction(signature, {
-        maxSupportedTransactionVersion: 0,
-      });
-    }
-
-    await new Promise((resolve) => setTimeout(resolve, delay));
-  }
-
-  throw new Error("Transaction not found after waiting");
-};
-
 const transfer = async (args: TransferPayload): Promise<IResponse> => {
   const connection = getConnection(args.rpcUrl);
 
@@ -173,6 +151,8 @@ const transfer = async (args: TransferPayload): Promise<IResponse> => {
     const from = solanaWeb3.Keypair.fromSecretKey(secretKey, {
       skipValidation: true,
     });
+
+    const { blockhash } = await connection.getLatestBlockhash();
 
     if (args.tokenAddress) {
       // SPL Token Transfer
@@ -195,33 +175,51 @@ const transfer = async (args: TransferPayload): Promise<IResponse> => {
         recipient
       );
 
-      signature = await transferToken(
-        connection,
-        from,
-        fromTokenAccount.address,
-        recipientTokenAccount.address,
-        from.publicKey,
-        solanaWeb3.LAMPORTS_PER_SOL * args.amount
+
+      const amount = parseAmount(args.amount, mint.decimals);
+      const tx = new solanaWeb3.Transaction().add(
+        createTransferInstruction(
+          fromTokenAccount.address,
+          recipientTokenAccount.address,
+          from.publicKey, // owner (signer)
+          amount,
+        ),
       );
+
+      tx.recentBlockhash = blockhash;
+      tx.feePayer = from.publicKey;
+      tx.sign(from);
+
+      // send and confirm
+      signature = await solanaWeb3.sendAndConfirmTransaction(connection, tx, [from], {
+        commitment: 'confirmed',
+      });
     } else {
       // Native SOL Transfer
+      const amount = parseAmount(args.amount, 9); // SOL always 9 decimals
       const transaction = new solanaWeb3.Transaction().add(
         solanaWeb3.SystemProgram.transfer({
           fromPubkey: from.publicKey,
           toPubkey: recipient,
-          lamports: solanaWeb3.LAMPORTS_PER_SOL * args.amount,
+          lamports: amount,
         })
       );
 
-      signature = await solanaWeb3.sendAndConfirmTransaction(
-        connection,
-        transaction,
-        [from]
-      );
+      transaction.recentBlockhash = blockhash;
+      transaction.feePayer = from.publicKey;
+      transaction.sign(from);
+
+      signature = await solanaWeb3.sendAndConfirmTransaction(connection, transaction, [from], {
+        commitment: 'confirmed',
+      });
     }
 
-    // wait transaction status before sending to response object
-    const tx = await waitForTransaction(connection, signature);
+    const tx = await connection.getTransaction(signature, {
+      maxSupportedTransactionVersion: 0,
+      commitment: 'confirmed',
+    });
+
+    console.log(tx);
 
     return successResponse({
       ...tx
