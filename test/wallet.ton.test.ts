6b72bbf1bb68dabaeaad8536595fc2db6811a08a
reactmore <reactmorecom@gmail.com>
2025-09-26 21:02:38 +0700

    added documentation

--- test/wallet.ton.test.ts
+++ test/wallet.ton.test.ts
@@ -22,254 +22,247 @@ describe('MultichainCryptoWallet Ton tests', () => {
   const expectedAddress12v4 = "UQAa9bnNsobIO5PwrLr7oKiLHk2GBzBNEAIYIjL_FMpwP7M-";
   const expectedAddress12v5 = "UQAqnG6hJtc6Vp_T6rezHHQG_d1v4HCwuvQDM4VKYrlwvon3";
   const privateKey12 = "d5eb32f5eea03e446eee06401d2de6cc146a9768c8c614a298d556e86773d3edcaab63276f86daa2d3fba585f712c146eb2aa1494aad61535810a31eaf245b6a"
-  // console.log("getBalance testnet Jetton token:\n", JSON.stringify(data, null, 2));
 
+  it('generateMnemonic', () => {
+    const mnemonic = generateMnemonic();
 
-  // it('generateMnemonic', () => {
-  //   const mnemonic = generateMnemonic();
+    expect(typeof mnemonic).toBe('string');
+  });
 
-  //   expect(typeof mnemonic).toBe('string');
-  // });
+  it('createWallet V4', async () => {
+    const wallet = await createWallet({
+      cluster: 'testnet',
+      network: 'ton',
+    });
 
-  // it('createWallet V4', async () => {
-  //   const wallet = await createWallet({
-  //     cluster: 'testnet',
-  //     network: 'ton',
-  //   });
+    expect(typeof wallet).toBe('object');
+    expect(wallet).toHaveProperty('address');
+    expect(wallet).toHaveProperty('privateKey');
+    expect(wallet).toHaveProperty('mnemonic');
+  });
 
-  //   expect(typeof wallet).toBe('object');
-  //   expect(wallet).toHaveProperty('address');
-  //   expect(wallet).toHaveProperty('privateKey');
-  //   expect(wallet).toHaveProperty('mnemonic');
-  // });
+  it('createWallet W5', async () => {
+    const wallet = await createWallet({
+      walletVersion: "W5",
+      cluster: 'testnet',
+      network: 'ton',
+    });
 
-  // it('createWallet W5', async () => {
-  //   const wallet = await createWallet({
-  //     walletVersion: "W5",
-  //     cluster: 'testnet',
-  //     network: 'ton',
-  //   });
+    expect(typeof wallet).toBe('object');
+    expect(wallet).toHaveProperty('address');
+    expect(wallet).toHaveProperty('privateKey');
+    expect(wallet).toHaveProperty('mnemonic');
+  });
 
-  //   expect(typeof wallet).toBe('object');
-  //   expect(wallet).toHaveProperty('address');
-  //   expect(wallet).toHaveProperty('privateKey');
-  //   expect(wallet).toHaveProperty('mnemonic');
-  // });
+  it('generateWalletFromMnemonic 12 V4', async () => {
+    const wallet = await generateWalletFromMnemonic({
+      mnemonic: mnemonic12,
+      walletVersion: "v4R2",
+      cluster: "mainnet",
+      network: 'ton',
+    });
 
-  // it('generateWalletFromMnemonic 12 V4', async () => {
-  //   const wallet = await generateWalletFromMnemonic({
-  //     mnemonic: mnemonic12,
-  //     walletVersion: "v4R2",
-  //     cluster: "mainnet",
-  //     network: 'ton',
-  //   });
+    expect(typeof wallet).toBe('object');
+    expect(wallet).toHaveProperty('address');
+    expect(wallet).toHaveProperty('privateKey');
+    expect(wallet).toHaveProperty('mnemonic');
+    expect(wallet.address).toBe(expectedAddress12v4);
+  });
 
-  //   expect(typeof wallet).toBe('object');
-  //   expect(wallet).toHaveProperty('address');
-  //   expect(wallet).toHaveProperty('privateKey');
-  //   expect(wallet).toHaveProperty('mnemonic');
-  //   expect(wallet.address).toBe(expectedAddress12v4);
-  // });
+  it('generateWalletFromMnemonic 12 W5', async () => {
+    const wallet = await generateWalletFromMnemonic({
+      mnemonic: mnemonic12,
+      walletVersion: "W5",
+      cluster: "mainnet",
+      network: 'ton',
+    });
 
-  // it('generateWalletFromMnemonic 12 W5', async () => {
-  //   const wallet = await generateWalletFromMnemonic({
-  //     mnemonic: mnemonic12,
-  //     walletVersion: "W5",
-  //     cluster: "mainnet",
-  //     network: 'ton',
-  //   });
+    expect(typeof wallet).toBe('object');
+    expect(wallet).toHaveProperty('address');
+    expect(wallet).toHaveProperty('privateKey');
+    expect(wallet).toHaveProperty('mnemonic');
+    expect(wallet.address).toBe(expectedAddress12v5);
+  });
 
-  //   expect(typeof wallet).toBe('object');
-  //   expect(wallet).toHaveProperty('address');
-  //   expect(wallet).toHaveProperty('privateKey');
-  //   expect(wallet).toHaveProperty('mnemonic');
-  //   expect(wallet.address).toBe(expectedAddress12v5);
-  // });
+  it('generateWalletFromMnemonic 24 V4', async () => {
+    const wallet = await generateWalletFromMnemonic({
+      mnemonic: mnemonic24,
+      walletVersion: "v4R2",
+      cluster: "testnet",
+      network: 'ton',
+    });
 
-  // it('generateWalletFromMnemonic 24 V4', async () => {
-  //   const wallet = await generateWalletFromMnemonic({
-  //     mnemonic: mnemonic24,
-  //     walletVersion: "v4R2",
-  //     cluster: "testnet",
-  //     network: 'ton',
-  //   });
+    expect(typeof wallet).toBe('object');
+    expect(wallet).toHaveProperty('address');
+    expect(wallet).toHaveProperty('privateKey');
+    expect(wallet).toHaveProperty('mnemonic');
+    expect(wallet.address).toBe(expectedAddress24v4);
+  });
 
-  //   console.log("generateWalletFromMnemonic 12 V4:\n", JSON.stringify(wallet, null, 2));
+  it('generateWalletFromMnemonic 24 W5', async () => {
+    const wallet = await generateWalletFromMnemonic({
+      mnemonic: mnemonic24,
+      walletVersion: "W5",
+      cluster: "testnet",
+      network: 'ton',
+    });
 
+    expect(typeof wallet).toBe('object');
+    expect(wallet).toHaveProperty('address');
+    expect(wallet).toHaveProperty('privateKey');
+    expect(wallet).toHaveProperty('mnemonic');
+    expect(wallet.address).toBe(expectedAddress24v5);
+  });
 
-  //   expect(typeof wallet).toBe('object');
-  //   expect(wallet).toHaveProperty('address');
-  //   expect(wallet).toHaveProperty('privateKey');
-  //   expect(wallet).toHaveProperty('mnemonic');
-  //   expect(wallet.address).toBe(expectedAddress24v4);
-  // });
+  it('getAddressFromPrivateKey12 V4', () => {
+    const address = getAddressFromPrivateKey({
+      privateKey: privateKey12,
+      walletVersion: "v4R2",
+      network: 'ton',
+    });
 
-  // it('generateWalletFromMnemonic 24 W5', async () => {
-  //   const wallet = await generateWalletFromMnemonic({
-  //     mnemonic: mnemonic24,
-  //     walletVersion: "W5",
-  //     cluster: "testnet",
-  //     network: 'ton',
-  //   });
+    expect(typeof address).toBe('object');
+    expect(address).toHaveProperty('address');
+    expect(typeof address.address).toBe('string');
+  });
 
-  //   expect(typeof wallet).toBe('object');
-  //   expect(wallet).toHaveProperty('address');
-  //   expect(wallet).toHaveProperty('privateKey');
-  //   expect(wallet).toHaveProperty('mnemonic');
-  //   expect(wallet.address).toBe(expectedAddress24v5);
-  // });
+  it('getAddressFromPrivateKey12 W5', () => {
+    const address = getAddressFromPrivateKey({
+      privateKey: privateKey12,
+      walletVersion: "W5",
+      network: 'ton',
+    });
 
-  // it('getAddressFromPrivateKey12 V4', () => {
-  //   const address = getAddressFromPrivateKey({
-  //     privateKey: privateKey12,
-  //     walletVersion: "v4R2",
-  //     network: 'ton',
-  //   });
+    expect(typeof address).toBe('object');
+    expect(address).toHaveProperty('address');
+    expect(typeof address.address).toBe('string');
+  });
 
-  //   expect(typeof address).toBe('object');
-  //   expect(address).toHaveProperty('address');
-  //   expect(typeof address.address).toBe('string');
-  // });
+  it('getAddressFromPrivateKey24 V4', () => {
+    const address = getAddressFromPrivateKey({
+      privateKey: privateKey24,
+      walletVersion: "v4R2",
+      network: 'ton',
+    });
 
-  // it('getAddressFromPrivateKey12 W5', () => {
-  //   const address = getAddressFromPrivateKey({
-  //     privateKey: privateKey12,
-  //     walletVersion: "W5",
-  //     network: 'ton',
-  //   });
+    expect(typeof address).toBe('object');
+    expect(address).toHaveProperty('address');
+    expect(typeof address.address).toBe('string');
+  });
 
-  //   expect(typeof address).toBe('object');
-  //   expect(address).toHaveProperty('address');
-  //   expect(typeof address.address).toBe('string');
-  // });
+  it('getAddressFromPrivateKey24 W5', () => {
+    const address = getAddressFromPrivateKey({
+      privateKey: privateKey24,
+      walletVersion: "W5",
+      network: 'ton',
+    });
 
-  // it('getAddressFromPrivateKey24 V4', () => {
-  //   const address = getAddressFromPrivateKey({
-  //     privateKey: privateKey24,
-  //     walletVersion: "v4R2",
-  //     network: 'ton',
-  //   });
+    expect(typeof address).toBe('object');
+    expect(address).toHaveProperty('address');
+    expect(typeof address.address).toBe('string');
+  });
 
-  //   expect(typeof address).toBe('object');
-  //   expect(address).toHaveProperty('address');
-  //   expect(typeof address.address).toBe('string');
-  // });
+  it('getBalance native TON V4', async () => {
+    const data = await getBalance({
+      address: expectedAddress24v4,
+      privateKey: privateKey24,
+      apiKey: tonCenterApiKey,
+      network: 'ton',
+      rpcUrl: tonCenterRpc,
+      walletVersion: "v4R2"
+    });
 
-  // it('getAddressFromPrivateKey24 W5', () => {
-  //   const address = getAddressFromPrivateKey({
-  //     privateKey: privateKey24,
-  //     walletVersion: "W5",
-  //     network: 'ton',
-  //   });
+    expect(typeof data).toBe('object');
+    expect(data).toHaveProperty('balance');
+    expect(typeof data.balance).toBe('string');
+  });
 
-  //   expect(typeof address).toBe('object');
-  //   expect(address).toHaveProperty('address');
-  //   expect(typeof address.address).toBe('string');
-  // });
+  it('getBalance native TON V5', async () => {
+    const data = await getBalance({
+      address: expectedAddress24v5,
+      privateKey: privateKey24,
+      apiKey: tonCenterApiKey,
+      network: 'ton',
+      rpcUrl: tonCenterRpc,
+      walletVersion: "W5",
+    });
 
-  // it('getBalance native TON V4', async () => {
-  //   const data = await getBalance({
-  //     address: expectedAddress24v4,
-  //     privateKey: privateKey24,
-  //     apiKey: tonCenterApiKey,
-  //     network: 'ton',
-  //     rpcUrl: tonCenterRpc,
-  //     walletVersion: "v4R2"
-  //   });
+    expect(typeof data).toBe('object');
+    expect(data).toHaveProperty('balance');
+    expect(typeof data.balance).toBe('string');
+  });
 
-  //   expect(typeof data).toBe('object');
-  //   expect(data).toHaveProperty('balance');
-  //   expect(typeof data.balance).toBe('string');
-  // });
+  it('getBalance testnet Jetton token', async () => {
+    const data = await getBalance({
+      address: expectedAddress24v4,
+      privateKey: privateKey24,
+      apiKey: tonCenterApiKey,
+      tokenAddress: 'kQB519C3IXFgCr4qKj6QrtaB9Pm3Sawr-Gonlo3O0cKL_I03',
+      network: 'ton',
+      rpcUrl: tonCenterRpc,
+    });
 
-  // it('getBalance native TON V5', async () => {
-  //   const data = await getBalance({
-  //     address: expectedAddress24v5,
-  //     privateKey: privateKey24,
-  //     apiKey: tonCenterApiKey,
-  //     network: 'ton',
-  //     rpcUrl: tonCenterRpc,
-  //     walletVersion: "W5",
-  //   });
+    expect(typeof data).toBe('object');
+    expect(data).toHaveProperty('balance');
+    expect(typeof data.balance).toBe('string');
+  });
 
-  //   expect(typeof data).toBe('object');
-  //   expect(data).toHaveProperty('balance');
-  //   expect(typeof data.balance).toBe('string');
-  // });
+  it('transfer TON V4', async () => {
+    const response = await transfer({
+      recipientAddress: expectedAddress24v5,
+      amount: 0.1,
+      memo: "Sending Ton to Wallet W5",
+      network: 'ton',
+      apiKey: tonCenterApiKey,
+      rpcUrl: tonCenterRpc,
+      privateKey: privateKey24,
+    });
 
-  // it('getBalance testnet Jetton token', async () => {
-  //   const data = await getBalance({
-  //     address: expectedAddress24v4,
-  //     privateKey: privateKey24,
-  //     apiKey: tonCenterApiKey,
-  //     tokenAddress: 'kQB519C3IXFgCr4qKj6QrtaB9Pm3Sawr-Gonlo3O0cKL_I03',
-  //     network: 'ton',
-  //     rpcUrl: tonCenterRpc,
-  //   });
+    expect(typeof response).toBe('object');
+  });
 
-  //   expect(typeof data).toBe('object');
-  //   expect(data).toHaveProperty('balance');
-  //   expect(typeof data.balance).toBe('string');
-  // });
+  it('transfer TON W5', async () => {
+    const response = await transfer({
+      recipientAddress: expectedAddress24v4,
+      amount: 0.1,
+      memo: "Sending Ton to Wallet V4",
+      network: 'ton',
+      apiKey: tonCenterApiKey,
+      rpcUrl: tonCenterRpc,
+      privateKey: privateKey24,
+      walletVersion: "W5",
+    });
 
-  // it('transfer TON V4', async () => {
-  //   const response = await transfer({
-  //     recipientAddress: expectedAddress24v5,
-  //     amount: 0.1,
-  //     memo: "Sending Ton to Wallet W5",
-  //     network: 'ton',
-  //     apiKey: tonCenterApiKey,
-  //     rpcUrl: tonCenterRpc,
-  //     privateKey: privateKey24,
-  //   });
+    expect(typeof response).toBe('object');
+  });
 
-  //   console.log("transfer TON V4:\n", JSON.stringify(response, null, 2));
+  it('transfer Jetton Token on Ton Testnet', async () => {
+    const response = await transfer({
+      recipientAddress: expectedAddress24v5,
+      tokenAddress: 'kQB519C3IXFgCr4qKj6QrtaB9Pm3Sawr-Gonlo3O0cKL_I03',
+      amount: 0.1,
+      forwardGas: 0, // remove for default Jetton Only
+      network: 'ton',
+      apiKey: tonCenterApiKey,
+      rpcUrl: tonCenterRpc,
+      privateKey: privateKey24,
+    });
 
-  //   expect(typeof response).toBe('object');
-  // });
+    expect(typeof response).toBe('object');
+  });
 
-  // it('transfer TON W5', async () => {
-  //   const response = await transfer({
-  //     recipientAddress: expectedAddress24v4,
-  //     amount: 0.1,
-  //     memo: "Sending Ton to Wallet V4",
-  //     network: 'ton',
-  //     apiKey: tonCenterApiKey,
-  //     rpcUrl: tonCenterRpc,
-  //     privateKey: privateKey24,
-  //     walletVersion: "W5",
-  //   });
+  it('getTransaction from hash', async () => {
+    const receipt = await getTransaction({
+      apiKey: tonCenterApiKey,
+      rpcUrl: tonCenterRpc,
+      address: expectedAddress24v5,
+      hash: '7a74b1e743f4251079afcb898f678c997820febb714103da417b2dd1370f968c',
+      logicalTime: '39457313000003',
+      network: 'ton',
+    });
 
-  //   expect(typeof response).toBe('object');
-  // });
-
-  // it('transfer Jetton Token on Ton Testnet', async () => {
-  //   const response = await transfer({
-  //     recipientAddress: expectedAddress24v5,
-  //     tokenAddress: 'kQB519C3IXFgCr4qKj6QrtaB9Pm3Sawr-Gonlo3O0cKL_I03',
-  //     amount: 0.1,
-  //     forwardGas: 0, // remove for default Jetton Only
-  //     network: 'ton',
-  //     apiKey: tonCenterApiKey,
-  //     rpcUrl: tonCenterRpc,
-  //     privateKey: privateKey24,
-  //   });
-
-  //   expect(typeof response).toBe('object');
-  // });
-
-  // it('getTransaction from hash', async () => {
-  //   const receipt = await getTransaction({
-  //     apiKey: tonCenterApiKey,
-  //     rpcUrl: tonCenterRpc,
-  //     address: expectedAddress24v5,
-  //     hash: '7a74b1e743f4251079afcb898f678c997820febb714103da417b2dd1370f968c',
-  //     logicalTime: '39457313000003',
-  //     network: 'ton',
-  //   });
-
-  //   expect(typeof receipt).toBe('object');
-  // });
+    expect(typeof receipt).toBe('object');
+  });
 
   it('getTokenInfo (Hipo Testnet Coin)', async () => {
     const tokenInfo = await getTokenInfo({
